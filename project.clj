5d3c14c7749d1b2dbf3639ffd36f7860d66aa524
Tsutomu YANO <t_yano@me.com>
2015-02-20 21:55:11 +0900

    changed version string.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-msgpack "0.1.0-SNAPSHOT"
+(defproject clojure-msgpack "0.1.1-SNAPSHOT"
   :description "Pure Clojure implementation of MessagePack spec."
   :url "https://github.com/edma2/clojure-msgpack"
   :license {:name "The MIT License (MIT)"
